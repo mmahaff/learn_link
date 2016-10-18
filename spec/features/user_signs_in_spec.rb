144f37f438d18d70b59522a511b28cc102c6457f
Mary Elaine <marymahaffey1@gmail.com>
2016-10-18 15:32:24 -0400

    Finished devise auth

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -9,7 +9,7 @@ feature 'user signs in' do
       click_link 'Sign In'
       fill_in 'Email', with: user.email
       fill_in 'Password', with: user.password
-      click_button 'Log in'
+      click_button 'Sign In'
 
       expect(page).to have_content('Sign Out')
       expect(page).to have_content('Welcome back!')
@@ -33,7 +33,7 @@ feature 'user signs in' do
       click_link 'Sign In'
       fill_in 'Email', with: user.email
       fill_in 'Password', with: 'dogcatmeowwoof'
-      click_button 'Log in'
+      click_button 'Sign in'
 
       expect(page).to have_content('Forgot your password?')
       expect(page).to_not have_content('Sign Out')
@@ -43,7 +43,7 @@ feature 'user signs in' do
       visit new_user_session_path
       fill_in 'Email', with: user.email
       fill_in 'Password', with: user.password
-      click_button 'Log in'
+      click_button 'Sign In'
 
       expect(page).to have_content('Sign Out')
       expect(page).to_not have_content('Sign In')
