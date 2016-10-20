ac452d3ea0a6a2d75ff2510118a48a8c6533b1ae
Mary Elaine <marymahaffey1@gmail.com>
2016-10-20 12:41:49 -0400

    Added integer column to users to associate with location

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161020154315) do
+ActiveRecord::Schema.define(version: 20161020163954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name_of_location", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -33,8 +37,11 @@ ActiveRecord::Schema.define(version: 20161020154315) do
     t.boolean  "is_student",                          null: false
     t.string   "city"
     t.string   "state"
+    t.integer  "location_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["location_id"], name: "index_users_on_location_id", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "users", "locations"
 end
