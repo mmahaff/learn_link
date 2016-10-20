6b9436948e73675e875f647110184d3ca9f52605
Mary Elaine <marymahaffey1@gmail.com>
2016-10-20 12:00:03 -0400

    Added state and city to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019225603) do
+ActiveRecord::Schema.define(version: 20161020154315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20161019225603) do
     t.string   "first_name"
     t.string   "last_name"
     t.boolean  "is_student",                          null: false
+    t.string   "city"
+    t.string   "state"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
