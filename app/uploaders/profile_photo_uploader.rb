9ef1f291c3146482930207ef7ca8cb08c1e6ce76
Mary Elaine <marymahaffey1@gmail.com>
2016-10-21 13:06:19 -0400

    Moving on from carrierwave for now

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -30,13 +30,25 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
+  version :large_profile_picture do
+    # returns a 150x150 image
+    process resize_to_fill: [150, 150]
+  end
+  version :medium_profile_picture do
+    # returns a 50x50 image
+    process resize_to_fill: [50, 50]
+  end
+  version :small_profile_picture do
+    # returns a 35x35 image
+    process resize_to_fill: [35, 35]
+  end
   # version :thumb do
   #   process resize_to_fit: [50, 50]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_whitelist
+  def extension_white_list
     %w(jpg jpeg gif png)
   end
 
