f68d4d18f50f6abff1ae9df09c365c4a0cc70652
Mary Elaine <marymahaffey1@gmail.com>
2016-10-20 21:52:07 -0400

    Set up carrierwave

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
+  mount_uploader :profile_photo, ProfilePhotoUploader
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
   validates_inclusion_of :is_student, in: [true, false]
+
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
