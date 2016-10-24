f371c357849f6f2c0dd72e1ec479452a2625de6d
Mary Elaine <marymahaffey1@gmail.com>
2016-10-24 12:43:05 -0400

    Rewrote test for user index and show page

--- spec/features/user_sees_user_list_spec.rb
+++ spec/features/user_sees_user_list_spec.rb
@@ -3,11 +3,36 @@ require 'rails_helper'
 feature 'user sees list of other users' do
   let!(:user) { FactoryGirl.create(:user) }
   let!(:user_2) { FactoryGirl.create(:user) }
+  let!(:user_3) { FactoryGirl.create(:user, is_student: false) }
+  let!(:user_4) { FactoryGirl.create(:user, is_student: false) }
 
-  scenario 'sees list of other users' do
+  scenario 'student sees list of tutors' do
+    login_as(user)
     visit users_path
 
+    expect(page).to have_content "List of Tutors"
+    expect(page).to have_content user_3.first_name
+    expect(page).to have_content user_3.city
+    expect(page).to have_content user_3.state
+    expect(page).to have_content user_3.location
+    expect(page).to have_content user_4.first_name
+    expect(page).to have_content user_4.city
+    expect(page).to have_content user_4.state
+    expect(page).to have_content user_4.location
+  end
+
+  scenario 'tutor sees list of students' do
+    login_as(user_3)
+    visit users_path
+
+    expect(page).to have_content "List of Students"
     expect(page).to have_content user.first_name
+    expect(page).to have_content user.city
+    expect(page).to have_content user.state
+    expect(page).to have_content user.location
     expect(page).to have_content user_2.first_name
+    expect(page).to have_content user_2.city
+    expect(page).to have_content user_2.state
+    expect(page).to have_content user_2.location
   end
 end
