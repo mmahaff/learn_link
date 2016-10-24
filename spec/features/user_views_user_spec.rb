a2dfe45c9ad612bf7b3c953549b9de8d5cfa7398
Mary Elaine <marymahaffey1@gmail.com>
2016-10-24 12:45:21 -0400

    Added test for user show page

--- spec/features/user_views_user_spec.rb
+++ spec/features/user_views_user_spec.rb
@@ -10,7 +10,6 @@ feature 'authenticated user visits a user profile page' do
 
     login_as(user)
     visit users_path
-    save_and_open_page
 
     click_link user_2.first_name
     expect(page).to have_content user_2.first_name
@@ -19,5 +18,18 @@ feature 'authenticated user visits a user profile page' do
     expect(page).to have_content user_2.city
     expect(page).to have_content user_2.state
     expect(page).to have_content user_2.email
+  end
+  scenario 'an authenticated tutor clicks link and is taken to show page for given student' do
+
+    login_as(user_2)
+    visit users_path
+
+    click_link user.first_name
+    expect(page).to have_content user.first_name
+    expect(page).to have_content user.last_name
+    expect(page).to have_content user.location
+    expect(page).to have_content user.city
+    expect(page).to have_content user.state
+    expect(page).to have_content user.email
   end
 end
