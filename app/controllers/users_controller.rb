9c2ed238f5134467d9bd23d8c105d2fd044d56e6
Mary Elaine <marymahaffey1@gmail.com>
2016-10-24 14:09:26 -0400

    Added carrierwave with failing test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,8 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :is_student, :city, :state, :location)
+    params.require(:user).permit(:first_name, :last_name, :email, :is_student,
+    :city, :state, :location, :about_me, :profile_photo, :subject, :language)
   end
 
   protected
---
+++ spec/features/profile_photo_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+include Warden::Test::Helpers
+
+feature 'an authenticated user can upload a profile_photo' do
+  let!(:user) { FactoryGirl.create(:user) }
+
+  scenario 'user uploads a profile photo' do
+    login_as(user)
+    visit edit_user_path(user.id)
+
+    fill_in 'City', with: 'Boston'
+    select 'Massachusetts', from: 'State'
+    fill_in 'Home Library', with: 'Boston Public Library'
+    fill_in 'Tell us about yourself', with: 'Just a little about myself'
+    attach_file :profile_photo, '#{Rails.root}/spec/support/images/test_profile.jpg'
+    click_button 'Edit'
+
+    expect(page).to have_content user.first_name
+    expect(page).to have_content user.last_name
+    expect(page).to have_content user.city
+    expect(page).to have_content user.location
+    expect(page).to have_content user.about_me
+    expect(page).to have_xpath("//img[contains(@src,'test_profile.jpg')]")
+  end
+end
