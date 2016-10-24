8beead37f2913e08fbe7884d98b6f8b227e1d1b9
Mary Elaine <marymahaffey1@gmail.com>
2016-10-24 15:52:31 -0400

    Passing carrierwave test

--- spec/features/profile_photo_spec.rb
+++ spec/features/profile_photo_spec.rb
@@ -12,14 +12,14 @@ feature 'an authenticated user can upload a profile_photo' do
     select 'Massachusetts', from: 'State'
     fill_in 'Home Library', with: 'Boston Public Library'
     fill_in 'Tell us about yourself', with: 'Just a little about myself'
-    attach_file :profile_photo, '#{Rails.root}/spec/support/images/test_profile.jpg'
+    attach_file 'user_profile_photo', "#{Rails.root}/spec/support/images/test_profile.jpg"
     click_button 'Edit'
 
+    expect(page).to have_xpath("//img[contains(@src, 'test_profile.jpg')]")
     expect(page).to have_content user.first_name
     expect(page).to have_content user.last_name
     expect(page).to have_content user.city
     expect(page).to have_content user.location
     expect(page).to have_content user.about_me
-    expect(page).to have_xpath("//img[contains(@src,'test_profile.jpg')]")
   end
 end
