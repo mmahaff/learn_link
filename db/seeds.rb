9bee1da71f9587feb514c0ca457b24a16e335778
Mary Elaine <marymahaffey1@gmail.com>
2016-10-26 15:41:07 -0400

    Added styling to convo pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,3 +32,30 @@ state: "MA", location: "Cambridge Public Library")
 
 user_7 = User.create!(first_name: "Hermione", last_name: "Granger", email: "example7@example.com",
 password: 'foobar7', password_confirmation: 'foobar7', is_student: false)
+
+user_8 = User.create!(first_name: "Michael", last_name: "Scott", email: "example8@example.com",
+password: 'foobar', password_confirmation: 'foobar', is_student: true, city: "Scranton",
+state: "PA", location: "Scranton Public Library")
+
+user_9 = User.create!(first_name: "Pam", last_name: "Beasley", email: "example9@example.com",
+password: 'foobar', password_confirmation: 'foobar', is_student: false, city: "Boston",
+state: "MA", location: "Boston Public Library")
+
+user_10 = User.create!(first_name: "Richard", last_name: "Wylie", email: "example10@example.com",
+password: 'foobar', password_confirmation: 'foobar', is_student: true, city: "Boston",
+state: "MA", location: "Boston Public Library")
+
+user_11 = User.create!(first_name: "Annie", last_name: "Woodworth", email: "example11@example.com",
+password: 'foobar', password_confirmation: 'foobar', is_student: false, city: "Boston",
+state: "MA", location: "Boston Public Library")
+
+user_12 = User.create!(first_name: "Neville", last_name: "Longbottom", email: "example12@example.com",
+password: 'foobar', password_confirmation: 'foobar', is_student: true, city: "Cambridge",
+state: "MA", location: "Cambridge Public Library")
+
+user_13 = User.create!(first_name: "Pavila", last_name: "Patel", email: "example13@example.com",
+password: 'foobar', password_confirmation: 'foobar', is_student: false, city: "Cambridge",
+state: "MA", location: "Cambridge Public Library")
+
+user_14 = User.create!(first_name: "Jim", last_name: "Halpert", email: "example14@example.com",
+password: 'foobar', password_confirmation: 'foobar', is_student: false)
