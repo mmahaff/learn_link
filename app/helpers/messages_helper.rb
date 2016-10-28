83fd4b6854d55cd11cdda3b6d091f288b7dd71c8
Mary Elaine <marymahaffey1@gmail.com>
2016-10-28 13:29:31 -0400

    fixed conversations issue

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -2,7 +2,7 @@ module MessagesHelper
   def recipients_options(chosen_recipient = nil)
     s = ''
     User.all.each do |user|
-      s << "<option value='#{user.id}' #{'selected' if user == chosen_recipient}>#{user.name}</option>"
+      s << "<option value= '#{user.id}' #{'selected' if user == chosen_recipient}>#{user.name}</option>"
     end
     s.html_safe
   end
