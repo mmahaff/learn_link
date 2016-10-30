c8a5bf44a3639fe89c0b8b87b11cdb87c67c3af7
Mary Elaine <marymahaffey1@gmail.com>
2016-10-30 13:29:49 -0400

    Trying to get JS working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,17 +15,7 @@
 //= require foundation
 //= require_tree .
 
-$(function(){
-  $(document).foundation(
-    orbit: {
-            animation: 'slide',
-            timer_speed: 1000,
-            pause_on_hover: true,
-            animation_speed: 500,
-            navigation_arrows: true,
-            bullets: false,
-            next_on_click: true
-          }
-  );
 
+$(function(){
+  $(document).foundation();
 });
--- spec/features/user_makes_post_spec.rb
+++ spec/features/user_makes_post_spec.rb
@@ -1,16 +1,28 @@
-# require 'rails_helper'
-# include Warden::Test::Helpers
-#
-# feature 'adding microposts' do
-#   let!(:user) {FactoryGirl.create(:user)}
-#   let!(:micropost) {FactoryGirl.create(:micropost)}
-#
-#   scenario 'authenticated user can make post to profile page' do
-#     login_as(user)
-#     visit user_path(current_user)
-#     fill_in 'Write new post', with: user.micropost.content
-#     click_button 'Post'
-#
-#     expect(page).to have_content user.micropost.content
-#   end
-# end
+require 'rails_helper'
+include Warden::Test::Helpers
+
+feature 'adding microposts' do
+  let!(:user) {FactoryGirl.create(:user)}
+
+  scenario 'authenticated user can make post to profile page' do
+    login_as(user)
+    visit root_path
+    fill_in 'micropost_content', with: "This is post 1"
+    click_button 'Post'
+
+    expect(page).to have_content "This is post 1"
+  end
+
+  scenario 'with latest post on top' do
+    login_as(user)
+    visit root_path
+    fill_in 'micropost_content', with: 'This is old post'
+    click_button 'Post'
+
+    fill_in 'micropost_content', with: 'This is new post'
+    click_button 'Post'
+
+    expect(page.body.index('This is new post')).to be < page.body.index('This is old post')
+  end
+
+end
