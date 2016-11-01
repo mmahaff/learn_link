48407b2a177b68ab35e2270cb096403200b9bfa9
Mary Elaine <marymahaffey1@gmail.com>
2016-11-01 16:34:13 -0400

    Setting default params[:page] in home#index if it's nil.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
 
   def index
     if user_signed_in?
+      params[:page] = 1 if params[:page].nil?
       @micropost = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
