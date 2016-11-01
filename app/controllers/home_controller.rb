e566e8484a86bdf966f2c9319788af6c4e13de44
Mary Elaine <marymahaffey1@gmail.com>
2016-11-01 16:41:33 -0400

    Rolling back last commit.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,6 @@ class HomeController < ApplicationController
 
   def index
     if user_signed_in?
-      params[:page] = 1 if params[:page].nil?
       @micropost = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
