28e375790ca63baf97ffb1f31bbfa2b468a2bbd7
Mary Elaine <marymahaffey1@gmail.com>
2016-11-10 10:50:56 -0500

    Deleted spaces and cleaned up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,5 +25,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
 end
